4b095e0ecd2739b48a5ab56a0ec839804cec3c1f
micro <github@olker.us>
2026-07-22 19:41:53 -0500

    Add customizeable start/stop callback logging

--- src/dash_helper/__init__.py
+++ src/dash_helper/__init__.py
@@ -1,1 +1,3 @@
-from .dash_helper import dash_helper, DashHelper, Input, State, Output, DashHelperGen, dash_helper_register
+from .dash_helper import dash_helper, DashHelper, Input, State, Output, DashHelperGen, dash_helper_register, \
+    TRIGGER_LOG_DEFAULT, TRIGGER_LOG_ALL, TRIGGER_LOG_DISPLAY_LABEL, TRIGGER_DISPLAY_INPUT, TRIGGER_DISPLAY_OUTPUT, \
+    TRIGGER_EXCLUDE, TRIGGER_LOG_FUNC_START, TRIGGER_LOG_FUNC_END
